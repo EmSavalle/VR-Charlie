de0373c9742f6b291b9aa8d1e9a4b0a359c64b54
EmSavalle <emile.savalle@gmail.com>
2019-09-29 15:59:15 +0200

    Init

---
+++ Assets/Scripts/Detected.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detected : MonoBehaviour {
+    public int timeUntilDetected = 5;
+    public int lookedAtCounter = 0;
+    private float lastSeen;
+    private int timeUntilReset = 3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > lastSeen + timeUntilReset)
+        {
+            lookedAtCounter = 0;
+        }
+	}
+
+    public void detected()
+    {
+        if (lookedAtCounter >= timeUntilDetected)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            lastSeen = Time.time;
+            lookedAtCounter += 1;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/HeadController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
+        if (Physics.Raycast(transform.position, forward, out hit))
+        {
+            Detected d = hit.transform.GetComponent<Detected>();
+            if (d)
+            {
+                d.detected();
+            }
+        }
+    }
+}
